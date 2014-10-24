159c1bd0fdf787caf1c46026462424f9be7f8054
Claus Lensbøl <cmol@cmol.dk>
2014-10-24 20:22:18 +0200

    Added fade for all lamps

--- lib/RGBController/lamp_control.rb
+++ lib/RGBController/lamp_control.rb
@@ -6,8 +6,9 @@ module RGBController
       @sock = TCPSocket.new opts[:address], opts[:port]
     end
 
-    def fade(lamps, length, color)
-      puts "fade"
+    def fade(lamps, length, cols)
+      data = [0b10000001, cols[0].to_i, cols[1].to_i, cols[2].to_i, length]
+      @sock.puts(data.pack("CCCCC"))
     end
 
     def set
