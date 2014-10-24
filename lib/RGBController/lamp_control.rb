89d0e555dda532a0d8351c4e9d7ba64b7f1c218a
Claus Lensbøl <cmol@cmol.dk>
2014-10-24 18:07:44 +0200

    Add initializer for server connection

--- lib/RGBController/lamp_control.rb
+++ lib/RGBController/lamp_control.rb
@@ -1,6 +1,11 @@
 module RGBController
   class LampControl
 
+    def initialize(opts = {})
+      # Connect to the server and save the socket
+      @sock = TCPSocket.new opts["address"], opts["port"]
+    end
+
     def fade
       raise NotYetImplemented
     end
