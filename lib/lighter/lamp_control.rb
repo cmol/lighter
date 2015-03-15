0f55abae8efacf18425965ab3b14439bd02d69d6
Claus Lensbøl <cmol@cmol.dk>
2015-03-15 17:15:01 +0100

    Let the controller accept a socket as argument

--- lib/lighter/lamp_control.rb
+++ lib/lighter/lamp_control.rb
@@ -3,21 +3,35 @@ module Lighter
 
     def initialize(opts = {})
       # Connect to the server and save the socket
-      @sock = TCPSocket.new opts[:address], opts[:port]
+      @sock = opts[:socket]
     end
 
     def fade(lamps, length, cols)
-      data = [mode(lamps,FADE), cols[0].to_i, cols[1].to_i, cols[2].to_i, length]
-      @sock.puts(data.pack("CCCCC"))
+      send(fadeData(lamps,length,cols))
     end
 
-    def set
-      raise NotYetImplemented
+    def set(lamps, cols)
+      send(setData(lamps, cols))
     end
+
+    private
 
     def mode(lamps, mode)
       raise ArgumentError if lamps < 0 || lamps > 4
       (lamps << 5) | mode
     end
+
+    def send(bytes)
+      @sock.puts(bytes.pack("C*"))
+    end
+
+    def fadeData(lamps, length, cols)
+      [mode(lamps,FADE), cols[0].to_i, cols[1].to_i, cols[2].to_i, length]
+    end
+
+    def setData(lamps, cols)
+      [mode(lamps,SET), cols[0].to_i, cols[1].to_i, cols[2].to_i]
+    end
   end
+
 end
