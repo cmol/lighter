fbc20daaaca7d1cc8bfcd270ead3c28d23f85df2
Claus Lensbøl <cmol@cmol.dk>
2015-03-17 16:41:35 +0100

    Add test for fade

--- test/test_lighter.rb
+++ test/test_lighter.rb
@@ -17,4 +17,13 @@ class TestLighter < MiniTest::Unit::TestCase
     assert mock.verify
   end
 
+  def test_fade
+    mock = MiniTest::Mock.new
+    mock.expect(:puts, nil, [[129,128,128,129,17].pack("C*")])
+    mock.expect(:puts, nil, [[65,17,17,17,77].pack("C*")])
+    lc = ::Lighter::LampControl.new(socket: mock)
+    lc.fade(4, 17, [128,128,129])
+    lc.fade(2, 77, [17,17,17])
+    assert mock.verify
+  end
 end
